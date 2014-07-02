6a408df49046622d8b517800512078cc2843c99e
John Pfeiffer <foupfeiffer@gmail.com>
2014-07-01 21:06:18 -0700

    major refactor to use a better theme, slightly cleaned up markdown, added about page

--- BLOG/content/my-first-blog-post.md
+++
@@ -1,8 +0,0 @@
-Title: My first blog post
-Date: 2014-06-21 20:20
-Tags: python
-Slug: my-first-blog-post
-Author: John Pfeiffer
-Summary: Short version for index and feeds
-
-This is the content of my first blog post. =)
--- BLOG/pelicanconf.py
+++ BLOG/pelicanconf.py
@@ -4,11 +4,10 @@ from __future__ import unicode_literals
 
 AUTHOR = u'john pfeiffer'
 SITENAME = u'johnpfeiffer'
-SITEURL = ''
-
+# SITEURL = 'johnpfeiffer.bitbucket.org'
 TIMEZONE = 'America/Los Angeles'
-
 DEFAULT_LANG = u'en'
+OUTPUT_PATH = '../'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -30,7 +29,16 @@ LINKS =  (('CV', 'https://www.linkedin.com/in/foupfeiffer'),
 
 STATIC_PATHS = ['images', 'js']
 # Sole author and don't use categories ... disable these features
-AUTHOR_SAVE_AS = False
-AUTHORS_SAVE_AS = False
-CATEGORY_SAVE_AS = False
-CATEGORIES_SAVE_AS = False
+# AUTHOR_SAVE_AS = False
+# AUTHORS_SAVE_AS = False
+# CATEGORY_SAVE_AS = False
+# CATEGORIES_SAVE_AS = False
+
+THEME = "/usr/local/lib/python2.7/dist-packages/pelican/themes/foundation-default-colours"
+FOUNDATION_FRONT_PAGE_FULL_ARTICLES = False
+FOUNDATION_ALTERNATE_FONTS = False
+FOUNDATION_TAGS_IN_MOBILE_SIDEBAR = False
+FOUNDATION_NEW_ANALYTICS = False
+FOUNDATION_ANALYTICS_DOMAIN = ''
+FOUNDATION_FOOTER_TEXT = ''
+FOUNDATION_PYGMENT_THEME = 'monokai'
